3a818c390a19b2a67a3a50de74eb78126107034f
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-09-19 18:49:38 +0600

    validate add money

--- js/home.js
+++ js/home.js
@@ -4,5 +4,22 @@ document
     event.preventDefault();
     const addMoneyInput = document.getElementById("input-add-money").value;
     const pinNumberInput = document.getElementById("input-pin-number").value;
-    console.log(addMoneyInput, pinNumberInput);
+    // console.log(addMoneyInput, pinNumberInput);
+
+    if (pinNumberInput === "1234") {
+      console.log("Added money to your account");
+
+      const balance = document.getElementById("account-balance").innerText;
+
+      const addMoneyNumber = parseFloat(addMoneyInput);
+      const balanceNumber = parseFloat(balance);
+      console.log(balanceNumber)
+
+      const newBalance = addMoneyNumber + balanceNumber;
+      console.log(newBalance);
+
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      console.log("Failed to add money! Please try again");
+    }
   });
